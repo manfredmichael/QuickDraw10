3acc1fd251b4c8c698a71cdb79cd5fbaa70dbde3
Manfred Michael <57741535+manfredmichael@users.noreply.github.com>
2020-11-02 22:21:19 +0700

    Update quick_draw10.py
    - Changed function name
    - Make splitting data only an option
    - Make different print statements for unsplitted data

--- quick_draw10.py
+++ quick_draw10.py
@@ -4,7 +4,7 @@ import urllib.request
 import os 
 import numpy as np
 
-def download_and_load(test_split = 0.2, max_items_per_class = 10000):
+def load_data(test_split = None, max_items_per_class = 10000):
   root = 'data'
   if not os.path.isdir(root):
     os.mkdir('data')
@@ -40,15 +40,21 @@ def download_and_load(test_split = 0.2, max_items_per_class = 10000):
   #reshape and inverse the colors 
   x = 255 - np.reshape(x, (x.shape[0], 28, 28))
 
-  #separate into training and testing 
-  test_size  = int(x.shape[0]/100*(test_split*100))
+  if test_split:
+    #separate into training and testing 
+    test_size  = int(x.shape[0]/100*(test_split*100))
 
-  x_test = x[0:test_size, :]
-  y_test = y[0:test_size]
+    x_test = x[0:test_size, :]
+    y_test = y[0:test_size]
 
-  x_train = x[test_size:x.shape[0], :]
-  y_train = y[test_size:y.shape[0]]
-  
-  print('Training Data : ', x_train.shape[0])
-  print('Testing  Data : ', x_test.shape[0])
-  return x_train, y_train, x_test, y_test, class_names
+    x_train = x[test_size:x.shape[0], :]
+    y_train = y[test_size:y.shape[0]]
+    
+    print('Training Data : ', x_train.shape[0])
+    print('Testing  Data : ', x_test.shape[0])
+    return x_train, y_train, x_test, y_test, class_names
+
+  else:
+    print('Image shape :', x.shape[1:])
+    print('Data :', x.shape[0])
+    return x, y, class_names
